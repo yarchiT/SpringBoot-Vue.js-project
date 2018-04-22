6b2dc516cdddf013d9e803c492d8f2f8bd54a1e1
Max Hetman <maxxhetman@gmail.com>
2018-04-22 15:55:39 +0300

    following users sidebar

--- frontend/src/components/user/timeline.vue
+++ frontend/src/components/user/timeline.vue
@@ -1,13 +1,46 @@
 <template>
   <div>
-
+    <following-users-sidebar v-bind:following-users="followingUsers"></following-users-sidebar>
     <p>Motherfuckers timeline</p>
   </div>
 </template>
 
 <script>
+  import FollowingUsersSidebar from "../followingUsersSidebar/followingUsersSidebar";
+  import axios from 'axios';
+
   export default {
-    name: 'timeline' //this is the name of the component
+    components: {FollowingUsersSidebar},
+    name: 'timeline', //this is the name of the component,
+    data() {
+      return {
+        followingUsers: [
+          {
+            nickName: 'slava',
+            avatarUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHjp65ReAyvAqAZv-AEAQDFh-v9eiOcit3a98sC5ME-uyKo_7qkA'
+          },
+          {
+            nickName: 'max',
+            avatarUrl: 'https://www.w3schools.com/w3images/avatar2.png'
+          },
+          {
+            nickName: 'yarik',
+            avatarUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHjp65ReAyvAqAZv-AEAQDFh-v9eiOcit3a98sC5ME-uyKo_7qkA'
+          }
+        ]
+      }
+    },
+    created() {
+      axios.get(`http://jsonplaceholder.typicode.com/posts`)
+        .then(response => {
+          // JSON responses are automatically parsed.
+          console.log('fetch followers data')
+          //this.followersList = response.data;
+        })
+        .catch(e => {
+          console.log('err');
+        })
+    }
   }
 </script>
 <style>
